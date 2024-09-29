a14134147ae527936509bb9a4169f210c4b4325f
Antoine <antoinebazin34@gmail.com>
2024-09-29 19:00:45 +0200

    fix

--- src/app/recents/page.tsx
+++ src/app/recents/page.tsx
@@ -1,11 +1,15 @@
+import Loader from "@/components/Loader";
 import { RecentVideoList } from "@/components/RecentVideoList";
+import { Suspense } from "react";
 
 export default function RecentsPage(): JSX.Element {
   return (
     <div className="container mx-auto px-4 py-8 flex flex-col min-h-screen">
       <h1 className="text-3xl font-bold mb-6 text-center">Recent Videos</h1>
       <div className="flex-grow flex items-center justify-center">
-        <RecentVideoList />
+        <Suspense fallback={<Loader />}>
+          <RecentVideoList />
+        </Suspense>
       </div>
     </div>
   );
