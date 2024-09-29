55c6d9fdf02076502fbab57141c439830873fbdf
Antoine <antoinebazin34@gmail.com>
2024-09-29 19:03:43 +0200

    auth

--- src/app/recents/page.tsx
+++ src/app/recents/page.tsx
@@ -1,8 +1,9 @@
 import Loader from "@/components/Loader";
 import { RecentVideoList } from "@/components/RecentVideoList";
 import { Suspense } from "react";
+import withAuth from "../hocs/withAuth";
 
-export default function RecentsPage(): JSX.Element {
+function RecentsPage(): JSX.Element {
   return (
     <div className="container mx-auto px-4 py-8 flex flex-col min-h-screen">
       <h1 className="text-3xl font-bold mb-6 text-center">Recent Videos</h1>
@@ -14,3 +15,5 @@ export default function RecentsPage(): JSX.Element {
     </div>
   );
 }
+
+export default withAuth(RecentsPage);
